32df7dc6a75905da4ebc837dedfb0262d8d3b549
Mansejal <77381477+Mansejal@users.noreply.github.com>
2021-06-15 12:12:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+status="";
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+
+    video = createCapture(VIDE0);
+    video.size(380,380);
+    video.hide();
+}
+function start() {
+    
+}
